49c3be99f8f8560d7373488f9c146f1121c4c619
Zameer Sayed <zameer8589@gmail.com>
2020-10-04 18:26:38 -0600

    commit Objects Practice

--- index.js
+++ index.js
@@ -1,152 +1,180 @@
-/**
- * Press / two stars and enter to build comments section
- * STARTUP 3 - Objects, Variable Declaration Assginment, Data Types , Premitive Type , Reference Type
- */
-console.log("Connected")
-console.log('String in single quotes')
-console.log("String in double quotes")
-console.log(10) //Number
-console.log('10') //String
-console.log(5.7)
-    //numbers in javascript dont divide into float, decimal etc its just number
-console.log(NaN) //Not a Number
-console.log(`Backtick and embeded expression calculating 4-4 = ${4-4}`)
+console.log({})
+    //{} is an Object
 
 
-console.log("Boolean")
-console.log(true)
-console.log(false)
-
-console.log(null) //indicator of abscence of any value.
-
-console.log(undefined) //never assign this value for any variable. used by JS internally
-
-console.log("Symbol")
-
-let a = Symbol("My Symbol")
-let b = Symbol("My Symbol")
-console.log(a)
-console.log(Symbol.iterator)
-console.log(a == a)
-console.log(a == b)
-console.log(a)
-console.log(Symbol("My Symbol") == Symbol("My Symbol"))
-
-
-
-//Reference Object Type
-console.log({}) //empty objectlet
-
-let newObject = { a: 10, b: true, c: "String" } //Object Literal Notation
-console.log(newObject)
-
-//Variable Declaration - 3 Types var, let , const
-//let and const introduced in ES6
-let a1 //declaration
-var b1 //declaration
-const c1 = 10 //declaration and assignment //with const delcare and assingment needs to happen at the same time
-b1 = true //assignment through an expression
-
-
-console.log(`a=${a1}`)
-console.log(`b=${b1}`)
-console.log(`c=${c1}`)
-
-//lets allows re-assign and const does not
-let a2 = 10
-
-//let a2 = 10 : not allowed, already declared - error
-a2 = 20
-
-const a3 = 10;
-//a3 = 20; Not possible, error
-
-
-//console.log(a4) //Reference error a4 is not defined
-
-let a4 //variable declared
-console.log(a4) // a4 is undefined as we have not assinged anything to a4 yet
-
-a4 = true
-
-console.log(a4) // not its assinged . console will display true
-
-// with let and var - declaration assings values as undefined.
-
-/**
- * /**
- * 
- * /**Exercise 1 : Variable Declaration and assignment
- * Declare variable "myObject1" and assigne value {}
- * Print this variable to the console
- * NOTE : variable "myObject1" will not be reassgined in the future
- * 
- ** /
- */
-
-
-const myObject1 = {}
-console.log(myObject1)
-
-
-/**
- * Exercise 2 : Variable Reusage
- * Declare variable x1 and assing value 10 to it.
- * Declare variable y1 and assing value true to it.
- * Declare myObjet2 and assing object with two name-vlaue pair and print
- * {a:10, b:true,_proto_:Object}
- * Declare "anotherObject"
- * Later assign value to it- Object with 3 name -value pairs
- * Print
- * {
- * newA: 20,
- * b: true,
- * c: {a:10, b:tue,_proto_:
- * Object},
- * _proto_:Object
- * }
- */
-
-let x1 = 10
-const y1 = true
-const myObject2 = {
-    a: x1,
-    b: y1, //adding comma is not required but is a best practise
+const myCity = {
+    city: "New york",
+    country: "USA"
 }
+
+
+//Object Modification
+//dot notation to access the value or change value of property
+
+console.log(myCity.city)
+myCity.city = "New Jersey"
+console.log(myCity.city)
+
+//adding properties to existing object using same dot notation
+myCity.popular = true
+console.log(myCity)
+
+//adding properties to existing object using same dot notation
+myCity["population"] = 102423424
+
+//difference between . and [] notation
+/**
+ * 
+ */
+
+const countryPropertyName = "state"
+myCity[countryPropertyName] = "NY"
+
+
+console.log(myCity)
+
+//rempoving property from existing object using delete
+//delete is an operator. 
+
+delete myCity.country
+console.log(myCity)
+
+
+//. first choice
+// if . not possible use [] when property names are stored in other variable[]
+
+
+
+//Nested Objects
+const myNewCity = {
+    city: "New York",
+    info: {
+        popular: true,
+        country: "USA",
+    }
+}
+console.log("myNewCity")
+console.log(myNewCity)
+
+console.log(myNewCity.info.popular)
+
+console.log(myNewCity.info["popular"])
+
+//Multiple rows withing same objects
+
+
+/**
+ * Exercice 1 
+ * 1. Create variable called myPost
+ * 2. Initial value should be empty object
+ * 3. Add Property called "postTitle" and value "Object is reference type"
+ * 4. Add one more property "postLikes" with value 0
+ * 5. Add third property "shared" and set its value to false
+ * 6. Increase value of the "postLikes" by 1
+ * 7. Delete propety "Shared"
+ */
+
+const myPost = {}
+myPost.postTitle = "Object is reference Type"
+myPost.postLikes = 0
+myPost.shared = false
+myPost.postLikes++;
+delete myPost.shared
+
+console.log(myPost)
+
+
+/**
+ * Excercise 2
+ * 1. Use Const for object declararion
+ * 2. Modify the value of object
+ */
+
+const myObject2 = {}
+myObject2.a = true
 console.log(myObject2)
-x1 = 20
-let anotherObject
-anotherObject = {
-    newA: x1,
-    b: y1,
-    c: myObject2,
-}
-console.log(anotherObject)
+
+// myObject2 = {
+//         a: true
+//     } //index.js:97 Uncaught TypeError: Assignment to constant variable.
+
+// you cannot change object varaible myObject2 as a whole.
+//but with const you can mutate the properties.
+
 
 
 /**
- * Javascript is a Dynamically Typed Language..value of other data types that be assigned to a variable
- * that
- * was orginally declared and assinged some other datatype
- * Type of the variable is set dynamically depending on the value
+ * Excercise 3
+ * 1. Create Variable called myObject3
+ * 2. Add property a with value 10
+ * 3. create another variable copyofmyObject3
+ * 5. its value should be myObject3
+ * 6. Add new property "b" with value false to the "copyOfMyObect"
  */
-let a5 = 10
-console.log(a5)
-console.log(typeof a5)
-a5 = "String"
-console.log(a5)
-console.log(typeof a5)
-a5 = true
-console.log(a5)
-console.log(typeof(a5))
 
 
-function afun() {
-    console.log("Hey there")
+let myObject3 = { a: 10 }
+console.log(myObject3)
+
+let copyOfMyObect = myObject3
+copyOfMyObect.b = false
+console.log(copyOfMyObect)
+console.log(myObject3)
+
+
+//pointer to the same obect in memory
+
+//What happens when you try to execute non-existing property of an Object
+
+
+console.log(myObject3.c) //undefined - Code execution is not stopped
+
+//code execution is stopped
+// console.log(v) //error when accessing non declared vaariable
+//     //Uncaught ReferenceError: v is not defined
+
+myObject3.newProperty = undefined
+    //dont use value as "undefined"  - IMPORTANT - this will unable us to indentify
+    //property exits in object or not use "null" instead
+
+console.log(myObject3)
+console.log(myObject3.myNewCity)
+
+/**
+ * Exercise 4
+ * 1. Create Obect - ObectwithNestedObject with initial value {}
+ * 2. Add property "nestedObject" with initial value {}
+ * 3. Add property "a" with value "null" to "nestedObject". Use dot notation
+ * 4. Add property "b" with value true to "nestedObject". Use bracket notation
+ * Create new variable with property name
+ */
+
+console.log("Exercise 4")
+const objectWithNestedObject = {}
+objectWithNestedObject.nestedObject = {}
+
+objectWithNestedObject.nestedObject.a = null
+
+const newPropertyName = "b"
+
+objectWithNestedObject.nestedObject[newPropertyName] = true
+
+console.log(objectWithNestedObject)
+
+
+
+/***********METHOD**************/
+//properties with function as a property value
+
+const myCityNew = {
+    city: "New York",
+    cityGreeting: function() {
+        console.log("Greetings!!")
+    }
 }
 
-afun()
+console.log(myCityNew)
+console.log(myCityNew.cityGreeting)
 
-afun = "String"
-
-//afun() - Uncaught SyntaxError: Unexpected end of input
+//Calling the method or function from the object.
+myCityNew.cityGreeting();
